478cd62e0731415dd84bd258153099bef8681b8a
hokogit <hothotsoccer@163.com>
2025-03-07 22:40:59 +0900

    添加沉默的羔羊

--- movies.js
+++ movies.js
@@ -3,32 +3,32 @@ const movies = [
     {
         id: 1,
         name: {
-            original: "Hannibal",
+            original: "Hannibal(2001)",
             chinese: "汉尼拔",
             japanese: "ハンニバル",
             english: "Hannibal"
         },
         country: "美国",
         language: "英语",
-        actors: "麦斯米科尔森",
-        plot: "恋爱脑的食人绅士的爱情故事。",
+        actors: "安东尼·霍普金斯",
+        plot: "《汉尼拔》（香港译《沉默的杀机》，新加坡、台湾译《人魔》）是一部2001年的美国犯罪心理惊悚电影，为《沉默的羔羊》的续集电影，改编自汤玛斯·哈里斯同名小说。导演是雷德利·斯科特，安东尼·霍普金斯饰演男主角汉尼拔·莱克特，朱丽安·摩尔饰演女主角FBI探员克丽丝·史达琳。该片赢得连续两周的北美票房冠军。为了不想再强化汉尼拔的魅力，电影改编后的结局与原著小说并不同。电影中女主角拒绝了汉尼拔，小说中最后女主角则与汉尼拔在一起。",
         date: "2025-2-28",
         link: "https://pan.quark.cn/s/d8e7bcd5ce40"
     },
     {
         id: 2,
         name: {
-            original: "霸王别姬",
-            chinese: "霸王别姬",
-            japanese: "さらば、わが愛",
+            original: "The Silence of the Lambs(1991)",
+            chinese: "沉默的羔羊",
+            japanese: "羊たちの沈黙",
             english: "Farewell My Concubine"
         },
-        country: "中国",
-        language: "汉语",
-        actors: "张国荣, 张丰毅, 巩俐",
-        plot: "讲述了两位京剧演员跨越半个世纪的悲欢离合，反映了中国社会的变迁。",
-        date: "2023-09-15",
-        link: "-"
+        country: "美国",
+        language: "英语",
+        actors: "朱迪·福斯特, 安东尼·霍普金斯",
+        plot: "《沉默的羔羊》（新加坡公映时译《迷离谋杀》），是于1991年上映的美国犯罪心理惊悚片，导演为乔纳森·戴米。内容改编自托马斯·哈里斯于1988年的同名小说，描述一名联邦调查局实习女干员捕捉一名连环杀手的经过。",
+        date: "2025-03-08",
+        link: "https://pan.quark.cn/s/2d4628e1893f"
     },
  
 ];
